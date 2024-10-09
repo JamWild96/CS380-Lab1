1e454ac14d4ccde6640bb7797b93b38e9acb8122
JamWild96 <100748711+JamWild96@users.noreply.github.com>
2024-10-08 21:25:58 -0700

    add find function

--- TreeDemov1.java
+++ TreeDemov1.java
@@ -97,14 +97,23 @@ class Node{
 	   }
 
 
-
-	   /*
-	   a method to find the node in the tree
-	   with a specific value
-	   */
+	/**
+     * Searches for a node with the given key in the binary search tree.
+     * @param root the root node of the tree
+     * @param key the value to search for
+     * @return true if the node with the key is found, false otherwise
+     */
 	   public boolean find(Node root, int key){
 		 //implement in here
-
+			if(root == null){
+					return false;
+				}else if (root.value == key){
+					return true;
+				}else if(key < root.value){
+					return find(root.left, key);
+				}else{
+					return find(root.right, key);
+					}
 	   }
 
 
